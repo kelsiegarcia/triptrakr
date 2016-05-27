e2fd88d7b0f9ca0332d6ef3f2b8b2bc3052fdda7
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-05-26 22:42:11 -0600

    trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -14,8 +14,8 @@ class TripsController < ApplicationController
 
   def create
     @trip = Trip.new(trip_params)
-    if trip.save
-      redirect_to trips_path(trip)
+    if @trip.save
+      redirect_to trips_path
     else
       render :new  
     end
