aa54a3c4639eb8b58efc0e426c692df93e644461
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-05-26 23:31:30 -0600

    address, location forms and back links

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -15,7 +15,7 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     if @trip.save
-      redirect_to trips_path
+      redirect_to trips_path(trip)
     else
       render :new  
     end
